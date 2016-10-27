be641642ac72fa5a292168d529b62024ad834832
rcjrman <rcjrman@yahoo.com>
2016-10-27 14:04:03 -0700

    Added browse capability
    Added the browse capability and updated the codes for the part I wanted
    to look for

--- AX99 Mapping.py
+++ AX99 Mapping.py
@@ -1,14 +1,29 @@
 import pandas	#Used to import into dataframe
 import warnings	#Using this to turn off warnings
 import glob		#Use Unix shell rules to find file names matching a pattern.
-import os
+import Tkinter as Tk
+import tkFileDialog
 
-warnings.simplefilter("ignore") #runs off UserWarning: Boolean Series key will be reindexed to match DataFrame index
+warnings.simplefilter("ignore") #Turns off UserWarning: Boolean Series key will be reindexed to match DataFrame index
+
+def browse_file():
+
+	root = Tk.Tk()
+	#fname = tkFileDialog.askopenfilename(filetypes = (("Template files", "*.type"), ("All files", "*")))
+	fname = tkFileDialog.askdirectory()
+	return fname
+	
+####################################################################################
+# Look through a directory and save all the csv files in a list
+path = browse_file()
+path += '/'
+my_file = glob.glob(path + 'Format*.csv')	
+####################################################################################
 
 ####################################################################################
 # Look through a directory and save all the csv files in a list
-path = 'C:/Users/rafael.colon/Desktop/Data/AX84/CFAR/CFAR 40076977/'
-my_file = glob.glob(path + 'Format*.csv')	
+#path = 'C:/Users/rafael.colon/Desktop/Data/AX84/CFAR/CFAR 40076977/'
+#my_file = glob.glob(path + 'Format*.csv')	
 ####################################################################################
 
 for entry in my_file:
@@ -30,30 +45,35 @@ for entry in my_file:
 	#Filter for the die we're looking for.
 	
 	a = my_df[['DIE_X','DIE_Y']]\
-		[my_df['ICGM code  '] == 1]\
-		[my_df['ZTC code  '] == 0]\
-		[my_df['VREF code  '] == 1]\
-		[my_df['OSC CLK code  '] == 0]\
-		[my_df['AMP OSC code  '] == 0]\
-		[my_df['AMP1 Vos  '] == 6]\
-		[my_df['AMP2 Vos  '] == 2]\
-		[my_df['IV_Xtalk R  '] == 18]\
-		[my_df['IV Xtalk F  '] == 16]\
-		[my_df['IV V Distortion  '] == 0]\
-		[my_df['IV I Distortion  '] == 18]\
-		[my_df['V offset  '] == 19]\
-		[my_df['I offset  '] == 133]\
-		[my_df['V gain  '] == 51]\
-		[my_df['I gain C  '] == 1]\
-		[my_df['I gain F  '] == 31]\
-		[my_df['Program Rev ID  '] == 0]\
-		[my_df['Spare  '] == 0]\
-		[my_df['Bst 1  '] == 55]\
-		[my_df['Bst Vout  '] == 3]\
-		[my_df['Bst Osc  '] == 19]\
-		[my_df['Bst Bias  '] == 6]\
-		[my_df['Bst Trim 5  '] == 3]\
-		[my_df['SAR  '] == 43]
+		[my_df['PTAT_Code  '] == 31]\
+		[my_df['GM_Code  '] == 1]\
+		[my_df['SD_Code  '] == 3]\
+		[my_df['ZTC_Code  '] == 31]\
+		[my_df['BG_Code  '] == 54]\
+		[my_df['BST_Gain_Code  '] == 0]\
+		[my_df['BST_VOS_Code  '] == 0]\
+		[my_df['BAT_Gain_Code  '] == 0]\
+		[my_df['BAT_VOS_Code  '] == 0]\
+		[my_df['OSC_Code  '] == 0]\
+		[my_df['I_Gain_Coarse_Code  '] == 20]\
+		[my_df['I_Gain_Fine_Code  '] == 30]\
+		[my_df['V_REF_Code  '] == 5]\
+		[my_df['IV_Correct_Code  '] == 0]\
+		[my_df['V_THD_Code  '] == 0]\
+		[my_df['I_THD_Code  '] == 19]\
+		[my_df['Spkr_VOS_Code  '] == 1]\
+		[my_df['IV_Half_I_Code  '] == 0]\
+		[my_df['BCNTIV_Code  '] == 0]\
+		[my_df['VOS_D_Code  '] == 4]\
+		[my_df['IV_XtalkCoarse_Code  '] == 0]\
+		[my_df['IV_XtalkFine_Code  '] == 19]\
+		[my_df['BOOST_ZC_Code  '] == 0]\
+		[my_df['BOOST_REF_Code  '] == 1]\
+		[my_df['Ilim_Gain_Code  '] == 3]\
+		[my_df['PreampOffsetAnalogMode  '] == 36]\
+		[my_df['Ilim_Offset_Code  '] == 60]\
+		[my_df['ALCEN_Option  '] == 1]\
+		[my_df['REV_ID_ILIM_Code  '] == 0]
 
 	print a
 	print '****************** END ************************'
