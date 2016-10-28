7c7c3d201b943e681ddd41d1435f1e08c1090e62
rcjrman <rcjrman@yahoo.com>
2016-10-28 15:41:32 -0700

    Adds test insertion to csv file
    Goes through and looks for groups of parts that were tested with the
    same insertion.

--- Create Insertion.py
+++ Create Insertion.py
@@ -20,7 +20,7 @@ def browse_file():
 # Look through a directory and save all the csv files in a list
 path = browse_file()
 path += '/'
-my_file = glob.glob(path + 'b*.csv')	
+my_file = glob.glob(path + '*.csv')	
 ####################################################################################
 	
 for entry in my_file:
@@ -33,12 +33,27 @@ for entry in my_file:
 		
 		with open(path + output_file, 'wb') as f_out:		
 			writer_in = csv.writer(f_out)
-			for row in reader_in:
-				#print len(row)
-				if len(row) != 0: #check if it's an empty line
-					if row[0][:3] == 'PID' or row[0] == 'Parameter':
-						if row[0] == 'Parameter':			#if it's the header row 
-							row.append('INSERTION')			#add column for insertion
+			insertion_index = 0								#set the insertion to zero at start of file
+			test_time = 0									#set the test time to zero at start of file
+			for row in reader_in:				
+				if len(row) != 0: 							#check if it's an empty line
+					if row[0] == 'Parameter':				#if it's the header row 
+						row.append('INSERTION')				#add column for insertion
+						writer_in.writerows([row])			#need [] otherwise it prints by character.	
+					
+					if row[0][:3] == 'PID':
+						if insertion_index == 0:			#indicates start of file
+							insertion_index += 1			#increase it to first insertion.
+							row.append(insertion_index)		#add the insertion to this row.
+							test_time = row[6]				#assign test to start the indexing
+						
+						elif row[6] == test_time:			#see if new row has same test time (same insertion)
+							row.append(insertion_index)		#add insertion index
+						else:
+							insertion_index += 1			#increase insertion index
+							row.append(insertion_index)		#add insertion index
+							test_time = row[6]				#assign test to start the indexing
+							
 						writer_in.writerows([row])			#need [] otherwise it prints by character.			
 		f_out.closed
 	f_in.closed
